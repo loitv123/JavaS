9a865dd98855f8e31c3d15b04826cae1cff52105
loitv123 <106915274+loitv123@users.noreply.github.com>
2023-09-13 20:04:57 +0700

    first commit

---
+++ firstday.js
@@ -0,0 +1,61 @@
+// let x = 10; 
+
+// if(x<5){
+//     let y = 10;
+//     console.log(y);
+// }else{
+//     let y = 15;
+//     console.log(y);
+// }
+
+//Global Scope
+
+// var x = "Atter is me";
+// let y = 1;
+// function testfunction(){
+//         if(y<2 ){
+//             console.log(x);
+//         }else{
+//             console.log(y)
+//         }
+// }
+// testfunction();
+// console.log(x);
+
+// ....> Function Scope
+
+// function Ex() {
+//     var ts = "hahaha"
+//     console.log(ts);
+//   }
+// Ex();
+
+
+//.......> Block Scope
+
+// function ex12() { 
+//     if(true){
+//         var lmao = "Co chac yeu la day"
+//         console.log(lmao);
+//     }
+//  }
+//  ex12();
+
+ // Assignment Operator
+
+//  let x = 14;
+//  console.log(x);
+//  x += 1;
+//  console.log(x);
+
+// In/Decrease Operator
+
+let a = 10;
+console.log(a);
+console.log(++a);
+console.log(a);
+console.log(--a);
+console.log(a++);
+console.log(a);
+console.log(a--);
+console.log(a);
